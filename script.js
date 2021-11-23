af01d8e3578e6875e1427dc6ba1a9d5808eb855f
Cristiano Sales <cristianosalesneves@gmail.com>
2021-11-22 22:38:14 -0300

    Requisito 7 com a ajuda de Douglas Marçal!

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-document.querySelector('.selected');
+// let firstSelected = document.querySelector('.selected');
 
+let mainColorPalette =  document.querySelector('#color-palette');
 
+function colorPicker(event) {
+    console.log(event.target);
+    if(event.target.id !== 'color-palette') {
+        let firstSelected = document.querySelector('.selected');
+        firstSelected.classList.remove('selected');
+        event.target.classList.add('selected');
+    }
+}
 
-
+mainColorPalette.addEventListener('click', colorPicker);
 
 
 
