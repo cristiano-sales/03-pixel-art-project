b69bb0603d51d5c2f1a2e3d6d50c030916794c16
Cristiano Sales <cristianosalesneves@gmail.com>
2021-11-23 11:52:29 -0300

    modifica principalmente o html e js tornando dinâmica a paleta de cores

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+for(let i = 0; i < 25; i += 1) {
+    let newContainer = document.createElement('div');
+    newContainer.className = 'pixel';
+    document.getElementById('pixel-board').appendChild(newContainer);
+}
+
 // let firstSelected = document.querySelector('.selected');
-
+// Referência: Douglas Marçal
 let mainColorPalette =  document.querySelector('#color-palette');
-
 function colorPicker(event) {
     console.log(event.target);
     if(event.target.id !== 'color-palette') {
@@ -10,8 +15,14 @@ function colorPicker(event) {
         event.target.classList.add('selected');
     }
 }
-
 mainColorPalette.addEventListener('click', colorPicker);
+
+// document.getElementById("pixel-board");
+// document.getElementsByClassName("pixel");
+
+// document.querySelector('#pixel-board');
+// document.querySelector('.pixel'); 
+// document.querySelectorAll('.pixel');
 
 
 
